eda2f27a1d9786f4fba000f6d01c9c253b15083f
Yureli201 <yurelimartinezmartinez@gmail.com>
2025-09-06 21:34:52 -0600

    Maquetación de navbar, login y register

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
