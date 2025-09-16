9182941a96cdc523c1454eefef1f8245247b8e4d
Yureli201 <yurelimartinezmartinez@gmail.com>
2025-09-16 13:18:04 -0600

    Implementación de protección de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,14 @@ import { Search } from './pages/search/search';
 import { ProductDetail } from './pages/product-detail/product-detail';
 import { Cart } from './pages/cart/cart';
 import { Profile } from './pages/profile/profile';
-
+import { Auth } from './core/guard/auth';
 
 export const routes: Routes = [
     { path: '', component : Home },
-    { path: 'login', component:Login },
-    { path: 'register', component: Register },
-    { path: 'search', component: Search },
+    { path: 'login', component:Login, canActivate: [Auth], data: { protected: false } },
+    { path: 'register', component: Register, canActivate: [Auth], data: { protected: false } },
     { path: 'search/:idCategory', component: Search },
     { path: 'product/:id', component: ProductDetail },
-    { path: 'cart', component: Cart },
-    { path: 'profile', component: Profile }
+    { path: 'cart', component: Cart, canActivate: [Auth], data: { protected: true } },
+    { path: 'profile', component: Profile, canActivate: [Auth], data: { protected: true } }
 ];
--- src/app/core/guard/auth.ts
+++ src/app/core/guard/auth.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
-
+import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { faL } from '@fortawesome/free-solid-svg-icons';
+import { CookieService } from 'ngx-cookie-service';
 @Injectable({
   providedIn: 'root'
 })
-export class Auth {
+export class Auth implements CanActivate {
+
+  constructor(private router: Router, private cookie: CookieService) { }
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const token = this.cookie.get('token')
+    const isProtected = route.data['protected'] === true;
+
+    
+    if (isProtected) {
+      //* Si hay token pasa
+      if (token) return true;
+
+      //* Si no hay token redirige al login
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    //*Si la ruta es para crear token
+    else {
+      //* Si hay token redirige al home
+      if (token) {
+        this.router.navigate(['/']);
+        return false;
+      }
+      //*Si no hay token pasa
+      return true;
+    }
+  }
+
+  //TODO: Verificar si el token es valido o no
   
 }
